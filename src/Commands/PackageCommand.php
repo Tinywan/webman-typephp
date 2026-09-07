7140adff2e988375ce6dae68498bbde11362949d
Tinywan <tinywan@gmail.com>
2026-09-07 09:42:14 +0800

    chore(release): bump default builder image to v0.0.16

--- src/Commands/PackageCommand.php
+++ src/Commands/PackageCommand.php
@@ -32,7 +32,7 @@ class PackageCommand extends Command
                 null,
                 InputOption::VALUE_REQUIRED,
                 'Docker builder image reference',
-                'tinywan/typephp-webman-builder:v0.0.15',
+                'tinywan/typephp-webman-builder:v0.0.16',
             )
             ->addOption(
                 'output-dir',
